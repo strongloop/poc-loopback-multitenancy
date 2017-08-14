063768927bd3a29b2e3c980bf4981427d7b77579
Rahul Radhakrishnan <rahulrad7@in.ibm.com>
2017-08-14 16:17:12 +0530

    Added option for adding tenant ID from header.

--- server/middleware/tenant-parser.js
+++ server/middleware/tenant-parser.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   return function tenantParser(req, res, next) {
-    req.tenantId = req.query.tenant || 1;
+    req.tenantId = req.query.tenant || (req.headers['x-tenant-id'] || 1);
 
     console.log('TR middleware triggered', req.tenantId);
 
